f2e087e5297135bb88f7b56d3121a2f1322d50d7
Seung, SaGong <79702151+ys05086@users.noreply.github.com>
2025-10-07 13:19:48 +0900

    Training script using cpu
    Added training script for RoR-3 model with data loading, training loop, and model saving functionality.

---
+++ main_cpu.py
@@ -0,0 +1,121 @@
+# i made the condition similar to the paper of RoR-3
+# iter is too much: spent 10 hours for training 100000 iteration.
+# but, iter in paper is almost 200000 in my learning( with random mini batch )
+# actually, random minibatch is not mini batch.
+# and batch_size =/= mini_batch size
+# btw paper's contidtion is this:
+#                                 Data Preprocessing  : subtract mean, devide std
+#                                 Data Augmentation   : flipping
+#                                 optimizer           : Adam
+#                                 weight_decay = 1e-4 : L2 regularization
+#                                 momentum = 0.9      : to add acceleration
+# but, Data Preprocessing is actually not needed
+import os
+import torch
+import numpy as np
+import cv2
+from tqdm import tqdm
+import function as ftn
+
+# user set param
+# num_training = 200000 # 200000
+learning_rate = 0.1
+model_save_path = '/ADL_project/PROJECT1_side/model/cpu/'
+batch_size = 1000
+restore_iter = 16400
+num_training = (50000 // batch_size) * 50 # 500 epochs with almost full data
+
+restore_lr = 0.01
+brestore = False
+
+# Load Data
+train_path = '/ADL_project/PROJECT1/CIFAR10/train/'
+test_path = '/ADL_project/PROJECT1/CIFAR10/test/'
+train_images, train_cls, train_mean, train_std = ftn.load_image(train_path, 50000)
+test_images, test_cls, test_mean, test_std = ftn.load_image(test_path, 1000, mean = train_mean, std = train_std)
+
+# bulid network
+model = ftn.RoRNet()
+
+# restore data
+if brestore == True:
+    print('Model Restore')
+    model.load_state_dict(torch.load(model_save_path + 'model_%d.pt' % restore_iter))
+
+# loss ftn and param settings
+optimizer = torch.optim.SGD(model.parameters(), weight_decay=1e-4, momentum=0.9, lr=restore_lr if brestore else learning_rate) # same as RoR paper
+loss = torch.nn.CrossEntropyLoss()
+
+model_ckpt = [] # iteration, acc, model_path
+
+# training Network
+for it in tqdm(range(restore_iter if brestore else 0, num_training), ncols=120, desc='Training Progress'):
+    # mini batch
+    batch_img, batch_cls = ftn.mini_batch_training(train_images, train_cls, batch_size)
+
+    model.train()
+    optimizer.zero_grad()
+    pred = model(torch.from_numpy(batch_img.astype(np.float32))) # (B,C,H,W)
+    cls_tensor = torch.tensor(batch_cls, dtype=torch.long)
+
+    train_loss = loss(pred, cls_tensor)
+    train_loss.backward()
+    optimizer.step()
+
+    if it % 100 == 0 and it > 0: # restore_iter: # 0 is for testing saving option
+        tqdm.write("\niteration: %d " % it)
+        tqdm.write("train loss : %f " % train_loss.item())
+        tqdm.write('Evaluating the Model...')
+        model.eval()
+
+        count = 0
+        for itest in tqdm(range(1000), ncols=100):
+            test_img = test_images[itest:itest + 1]
+            test_img = np.transpose(test_img, (0, 3, 1, 2)) # Change to [B, C, H, W]
+
+            with torch.no_grad():
+                pred = model(torch.from_numpy(test_img.astype(np.float32)))
+
+            pred = pred.numpy() # [1, 10]
+            pred = np.reshape(pred, 10)
+            pred = np.argmax(pred)
+
+            gt = test_cls[itest]
+
+            if int(gt) == int(pred):
+                count += 1
+
+        acc = count / 1000 * 100
+
+        tqdm.write("Accuracy   : %f " % acc)
+        tqdm.write("Current LR : %f " % optimizer.param_groups[0]['lr'])
+
+        tqdm.write('SAVING MODEL')
+        if not os.path.isdir(model_save_path):
+            os.mkdir(model_save_path)
+        torch.save(model.state_dict(), model_save_path + 'model_%d.pt' % it)
+        tqdm.write('MODEL SAVED')
+
+        # check point data save
+        model_ckpt.append((it, acc, model_save_path + 'model_%d.pt' % it))
+    
+    # more easy to read: not one line
+    if it == num_training // 2:
+        optimizer.param_groups[0]['lr'] = 0.01
+
+    if it == num_training * 3 // 4:
+        optimizer.param_groups[0]['lr'] = 0.001 # end is near here
+
+    if it == 220000:
+        optimizer.param_groups[0]['lr'] = 0.0001
+
+    # if it == 100000:
+    #     optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] * 0.1
+
+    # if it == 150000:
+    #     optimizer.param_groups[0]['lr'] = optimizer.param_groups[0]['lr'] * 0.1
+
+print('TRAINING DONE')
+print('BEST MODEL')
+model_ckpt = sorted(model_ckpt, key=lambda x: x[1], reverse=True)
+print(model_ckpt[0])
